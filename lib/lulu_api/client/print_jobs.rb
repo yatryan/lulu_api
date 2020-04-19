6ddba6a84d62b0c2d35cddd7edd32a748ac114ee
Taylor Ryan <taylor@orbweaver.com>
2020-04-18 23:49:38 -0400

    Run all responses through lulu handler

--- lib/lulu_api/client/print_jobs.rb
+++ lib/lulu_api/client/print_jobs.rb
@@ -6,35 +6,34 @@ module LuluApi
         fetch_token if @token.nil?
 
         response = self.class.get("/print-jobs", { query: options })
-        response.parsed_response["results"]
+        handle_lulu_response response
       end
 
       def get_print_job(id)
         fetch_token if @token.nil?
 
         response = self.class.get("/print-jobs/#{id}")
-        response.parsed_response
+        handle_lulu_response response
       end
 
       def get_print_job_costs(id)
         fetch_token if @token.nil?
 
         response = self.class.get("/print-jobs/#{id}/costs")
-        response.parsed_response
+        handle_lulu_response response
       end
 
       def get_print_job_status(id)
         fetch_token if @token.nil?
 
         response = self.class.get("/print-jobs/#{id}/status")
-        response.parsed_response
+        handle_lulu_response response
       end
 
       def create_print_job(job)
         fetch_token if @token.nil?
 
         response = self.class.post("/print-jobs", { body: job.to_json })
-        response.parsed_response
       end
     end
   end
